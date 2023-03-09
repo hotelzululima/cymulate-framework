3e4a8a3d2937217d0a35a95b5381c408622a12d0
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-09 17:43:16 +0800

    Fix logger

--- core/utils/log.py
+++ core/utils/log.py
@@ -1,11 +1,16 @@
+import sys
 from loguru import logger
 
 
 class Log:
-    def __init__(self, log_name, log_level):
-        self.log_name = log_name
+    def __init__(self, log_name: str, log_level: str):
+        self.log_file = f"{log_name}.log"
         self.log_level = log_level
 
     def get_logger(self):
-        logger.add(f"{self.log_name}", level=self.log_level, rotation="5 MB")
+        if self.log_level == "DEBUG":
+            logger.add(f"{self.log_file}", level="DEBUG", rotation="10 MB", backtrace=True, diagnose=True)
+        else:
+            logger.remove(0)
+            logger.add(sys.stderr, format="<level>{message}</level>", level="INFO", colorize=True)
         return logger
