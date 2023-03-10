2da979d1aa7824c5a10c6b3bd6b7d7b6c43a762e
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-10 18:27:08 +0800

    Finish windows execution module (roughly)

---
+++ core/module/base.py
@@ -0,0 +1,121 @@
+import json
+from core.model.execution import Execution, SuccessIndicator
+from dacite import from_dict
+from pathlib import Path
+from core.utils.log import Log
+
+
+class BaseModule:
+    """
+    Base class for all modules
+    """
+
+    def __init__(self, execution_id: str, debug: bool):
+        """
+        Name: Module Name
+        Description: Module Description
+        OS : Windows, Linux, Mac, Android, iOS
+        Platform: Powershell, Python
+        Elevation Requirement: True, False
+        Execution Timeout: N minutes
+        Tactics: ID of MITRE ATT&CK Tactics
+        Technique: ID of MITRE ATT&CK Technique
+        """
+        log_level = 'DEBUG' if debug else 'INFO'
+        self.logger = Log(log_name='module', log_level=log_level).get_logger()
+        self.execution_id = execution_id
+        execution_dict = self.get_execution(execution_id)
+        # Deserialize execution json into Execution object
+        self.execution = from_dict(data_class=Execution, data=execution_dict)
+        # Replace variable name in powershell script to actual value
+        self.input_arguments = self.get_input_arguments()
+
+    @staticmethod
+    def get_execution(execution_id: str) -> dict:
+        """
+        Get Execution dict from executions json file by id
+        """
+        execution_file = Path(__file__).parent.parent / 'assets/executions.json'
+        with open(execution_file, 'r', encoding='utf-8') as f:
+            execution_data = json.load(f)
+        execution = next((e for e in execution_data['data'] if e['_id'] == execution_id), None)
+        return execution
+
+    def get_enabled_success_indicator(self) -> SuccessIndicator:
+        """
+        Get the enabled success indicator
+        """
+        for indicator in self.execution.successIndicators:
+            if indicator.enabled:
+                return indicator
+
+    def get_input_arguments(self) -> dict:
+        """
+        Turn input arguments into a dict
+        """
+        return {arg.name: arg.default[0] for arg in self.execution.inputArguments}
+
+    def check_dependency(self) -> bool:
+        """
+        Check if dependencies are installed
+        """
+
+    def resolve_variable(self, command: str, input_arguments: dict = None) -> str:
+        """
+        Transfer ruby string format variable name to value
+        Replace input arguments in command -> #{test} -> test_value
+        """
+        input_arguments = input_arguments or self.input_arguments
+        for name, value in input_arguments.items():
+            command = command.replace(f'#{{{name}}}', value)
+        return command
+
+    def execute(self):
+        """
+        Main execution function
+        """
+        self.logger.info('***\nExecution Phrase\n***')
+
+    def success_indicate(self) -> bool:
+        """
+        Check if the output indicates success
+        """
+        pass
+
+    def cleanup(self):
+        """
+        Clean up after the scenario
+        """
+        pass
+
+    def output_parser(self, output: str) -> str:
+        """
+        Parse output result
+        """
+        pass
+
+    def run(self):
+        """
+        Run the module
+        """
+        module_info = f"""
+        Name: {self.execution.name}
+        Description: {self.execution.description}
+        OS : {", ".join(self.execution.os)}
+        Platform: {", ".join(self.execution.supportedPlatforms)}
+        Elevation Requirement: {self.execution.executor.elevationRequired}
+        Execution Timeout: {self.execution.timeout} minutes
+        Tactics: {", ".join([t['name'] for t in self.execution.tactics])}
+        Technique: {", ".join([t['name'] for t in self.execution.techniques])}
+        """
+        self.logger.info(f"Running module:\n---\n {module_info}\n---\n")
+        if self.check_dependency():
+            self.logger.success('All dependency checks passed')
+            self.execute()
+            if self.success_indicate():
+                self.logger.success('Module executed successfully')
+            else:
+                self.logger.error('Module executed unsuccessfully')
+        else:
+            self.logger.error('Module dependency check failed')
+        self.cleanup()
---
+++ core/module/windows.py
@@ -0,0 +1,71 @@
+"""
+Windows module for execution
+"""
+
+from core.module.base import BaseModule
+from core.utils.common import powershell, powershell_return_code
+
+
+class WindowsModule(BaseModule):
+    def __init__(self, execution_id: str, debug: bool = True):
+        super().__init__(execution_id=execution_id, debug=debug)
+
+    def check_dependency(self) -> bool:
+        failed_dependency = []
+        for dependency in self.execution.dependencies:
+            self.logger.info(f'Checking : {dependency.description}')
+            if dependency.dependencyExecutorName == 'powershell':
+                # Get-Pre-req command: Download files...etc
+                get_pre_req_cmd = self.resolve_variable(dependency.getPrereqCommand)
+                result = powershell(get_pre_req_cmd)
+                self.logger.debug(f'Get-Pre-req command result: {result}\n')
+
+                # Get-Pre-req command: Download files...etc
+                pre_req_cmd = self.resolve_variable(dependency.prereqCommand)
+                is_dependency_installed = powershell_return_code(pre_req_cmd) == 0
+                if not is_dependency_installed:
+                    self.logger.error(f'Failed this check: {dependency.description}')
+                    failed_dependency.append(dependency.description)
+                else:
+                    self.logger.success(f'Passed this check: {dependency.description}')
+        return len(failed_dependency) == 0
+
+    @staticmethod
+    def resolve_file_path(variable: str) -> str:
+        """
+        Resolve absolute path for the variable in powershell
+        """
+        return powershell(f'echo {variable}')[0].decode('big5').strip()
+
+    def set_input_arguments_abs(self):
+        """
+        Set input_arguments's values to resolve absolute path for the variable in powershell
+        """
+        input_arguments = self.get_input_arguments()
+        for name, value in input_arguments.items():
+            if value.startswith('$env'):
+                self.input_arguments[name] = self.resolve_file_path(value)
+
+    def execute(self):
+        if self.execution.executor.name == 'powershell':
+            # Not resolving absolute path at init because the temp path might change if evaluated to other users
+            self.set_input_arguments_abs()
+            powershell_script = self.resolve_variable(self.execution.executor.command)
+            self.logger.debug(f'Running Powershell Script: {powershell_script}\n')
+            result_list = powershell(powershell_script)
+            result = "\n".join([r.decode('big5') for r in result_list])
+            self.logger.debug(f'Powershell script result: {result}\n')
+
+    def success_indicate(self) -> bool:
+        success_flag = False
+        for success_indicator in self.execution.successIndicators:
+            if success_indicator.successIndicatorExecutor == "powershell":
+                powershell_script = self.resolve_variable(success_indicator.successIndicatorCommand)
+                result = powershell_return_code(powershell_script)
+                if result == 0:
+                    self.logger.success(f'Success Indicator: {success_indicator.description}')
+                    success_flag = True
+                    break
+                else:
+                    self.logger.warning(f'Failed Success Indicator: {success_indicator.description}')
+        return success_flag
