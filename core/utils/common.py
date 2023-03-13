22c1066cb9034b43d8f05d45ddb719181e39a36d
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-13 18:13:23 +0800

    Add function to gain admin priv

--- core/utils/common.py
+++ core/utils/common.py
@@ -1,6 +1,14 @@
 import subprocess
-from subprocess import PIPE, run
+import ctypes
+import sys
+from subprocess import PIPE
 from base64 import b64encode
+
+
+def gain_admin_priv():
+    """Gain admin priviledge if not running as admin."""
+    if ctypes.windll.shell32.IsUserAnAdmin() != 1:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
 
 def powershell_encode(cmd: str) -> str:
