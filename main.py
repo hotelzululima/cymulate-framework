693e8bceae82bc8f051895c3e7912d693b482a3e
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-13 18:14:27 +0800

    Debug "Scheduled Task - Persistance"

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from core.module.windows import WindowsModule
 
 
 if __name__ == '__main__':
-    m = WindowsModule("62385f03a0e69ed2274622cc", debug=False)
+    # Download and Execute Phishing Attachment - VBScript
+    # m = WindowsModule("62385f03a0e69ed2274622cc", debug=False)
+    # m.run()
 
-    # Template Main
+    # Invoke Raw Powershell command
+    # m = WindowsModule("628263ae9bad2dfa4b66d4c6")
+    # m.run()
+
+    # Scheduled Task - Persistance
+    m = WindowsModule("60f43effac529a1c1f25ebdc")
     m.run()
