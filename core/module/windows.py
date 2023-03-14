efb41d7d064fe6cba56a64e63c5e76c176ff42e4
opabravo <21262207+opabravo@users.noreply.github.com>
2023-03-14 11:14:23 +0800

    Add success_indicate for python scripts

--- core/module/windows.py
+++ core/module/windows.py
@@ -3,7 +3,7 @@ Windows module for execution
 """
 
 from core.module.base import BaseModule
-from core.utils.common import powershell, gain_admin_priv
+from core.utils.common import powershell, gain_admin_priv, python_exec
 
 
 class WindowsModule(BaseModule):
@@ -19,10 +19,10 @@ class WindowsModule(BaseModule):
             if dependency.dependencyExecutorName == 'powershell':
                 # Get-Pre-req command: Download files...etc
                 get_pre_req_cmd = self.resolve_variable(dependency.getPrereqCommand)
-                self.logger.debug(f'Running Powershell Script: {get_pre_req_cmd}\n')
+                self.logger.debug(f'Running Powershell Script: \n{get_pre_req_cmd}\n')
                 p = powershell(get_pre_req_cmd)
                 result = "\n".join([r.decode('big5') for r in p.communicate()])
-                self.logger.debug(f'Get-Pre-req command result: {result}\n')
+                self.logger.debug(f'Get-Pre-req command result: \n{result}\n')
 
                 # Get-Pre-req command: Download files...etc
                 pre_req_cmd = self.resolve_variable(dependency.prereqCommand)
@@ -62,11 +62,11 @@ class WindowsModule(BaseModule):
             # Not resolving absolute path at init because the temp path might change if evaluated to other users
             self.set_input_arguments_abs()
             powershell_script = self.resolve_variable(self.execution.executor.command)
-            self.logger.debug(f'Running Powershell Script: {powershell_script}\n')
+            self.logger.debug(f'Running Powershell Script: \n{powershell_script}\n')
             p = powershell(powershell_script)
             result_list = p.communicate()
             result = "\n".join([r.decode('big5') for r in result_list])
-            self.logger.debug(f'Powershell script result: {result}\n')
+            self.logger.debug(f'Powershell script result: \n{result}\n')
 
         self.execution_output = result_list[0].decode('big5')
         self.execution_return_code = p.returncode
@@ -82,10 +82,10 @@ class WindowsModule(BaseModule):
                 return False
 
         for success_indicator in self.execution.successIndicators:
+            script = self.resolve_variable(success_indicator.successIndicatorCommand)
             if success_indicator.successIndicatorExecutor == "powershell":
-                powershell_script = self.resolve_variable(success_indicator.successIndicatorCommand)
-                self.logger.debug(f'Running Powershell Script: {powershell_script}\n')
-                p = powershell(powershell_script)
+                self.logger.debug(f'Running Powershell Script: \n{script}\n')
+                p = powershell(script)
                 result = p.communicate()
                 self.logger.debug(f'Powershell script result: {result}\n')
                 if p.returncode == 0:
@@ -94,11 +94,26 @@ class WindowsModule(BaseModule):
                 else:
                     self.logger.warning(f'Failed Success Indicator: {success_indicator.description}')
             elif success_indicator.successIndicatorExecutor == "python":
-                python_script = self.resolve_variable(success_indicator.successIndicatorCommand)
-                python_script = python_script.replace('exit(0)', 'os._sexit(0)').replace('exit(1)', 'os._exit(1)')
+                # Replace exit(0) and exit(1) with exit_code = 0 and exit_code = 1
+                python_script = script.replace('exit(0)', '  return 0').replace('exit(1)', 'return 1')
+
                 # Append the execution output variable to the python script at the beginning
-                python_script = f"import os\npiped_output = {self.execution_output}\n" + python_script
-                self.logger.debug(f'Running Python Script: {python_script}\n')
-                exec(python_script)
-                pass
+                python_script = f"piped_output = '''{self.execution_output}'''\n" + python_script
+
+                # Make script to function
+                python_script = "\n    ".join(python_script.splitlines())
+                python_script = f"def py_test():\n    {python_script}"
+
+                # Append variable assignment to the python script at the end
+                python_script = python_script + "\nexit_code = py_test()"
+
+                self.logger.debug(f'Running Python Script: \n{python_script}\n')
+                result = python_exec(python_script)
+                self.logger.debug(f'Python script result: \n{result}\n')
+                if result.get('exit_code') == 0:
+                    self.logger.success(f'Success Indicator: {success_indicator.description}')
+                    return True
+                else:
+                    self.logger.warning(f'Failed Success Indicator: {success_indicator.description}')
+
         return False
