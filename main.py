a79fd0f62a8dfb67f240bbc634e13d996bf77834
Fate Walker <21262207+opabravo@users.noreply.github.com>
2023-03-15 16:41:25 +0800

    Update main.py
    Let entry point receive module ID and run it

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
+"""
+Entry point for the framework.
+"""
+import sys
+import core.template.apt34 as apt34
 from core.module.windows import WindowsModule
 
-
 if __name__ == '__main__':
-    # Download and Execute Phishing Attachment - VBScript
-    # m = WindowsModule("62385f03a0e69ed2274622cc", debug=False)
-    # m.run()
-
-    # Invoke Raw Powershell command
-    # m = WindowsModule("628263ae9bad2dfa4b66d4c6")
-    # m.run()
-
-    # Scheduled Task - Persistance
-    if len(sys.argv)>1:
-        m = WindowsModule(sys.argv[1])
+    if len(sys.argv) == 2:
+        WindowsModule(sys.argv[1], debug=True).run()
     else:
-        m = WindowsModule("60f43effac529a1c1f25ebdc")
-    m.run()
+        apt34.start()
